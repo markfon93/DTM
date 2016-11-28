e09b0a635e31005b7af6e6ae48c2ff486369c52d
Mark Fontaine <Mark Fontaine>
2016-11-28 10:39:01 -0800

    fixed tab/line spacing in php header/footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,11 @@
-
-	
 </body>
 <footer>
 	<div class="menubar" id="footerbar">
-				<li><a href="/">Home</a></li>
-				<li><a href="about.php">About Us</a></li>
-				<li><a href="contact.php">Contact Us</a></li>
-				<li><a href="products.php">Products</a></li>
-				<li><a href="form.php">Form</a></li>
+		<li><a href="/">Home</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a href="contact.php">Contact Us</a></li>
+		<li><a href="products.php">Products</a></li>
+		<li><a href="form.php">Form</a></li>
 	</div> 
 	<div id="copyright">
 	Copyright © Dynamic Threat Management. All Right Reserved.
--- includes/header.php
+++ includes/header.php
@@ -3,19 +3,17 @@
 	<title>Dynamic Threat Management : <?= $pageTitle ?> </title>
 	<meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="/stylesheets/main.css">
-		<div id="masthead">
-			<img src="/images/logo-horiz.png">		
-			<h1>Low Profile & Concealment Systems</h1>
-			<div class="menubar" id = "headerbar">
-				
-					<li><a href="/">Home</a></li>
-					<li><a href="about.php">About Us</a></li>
-					<li><a href="contact.php">Contact Us</a></li>
-					<li><a href="products.php">Products</a></li>
-					<li><a href="form.php">Form</a></li>
-			</div>
+	<div id="masthead">
+		<img src="/images/logo-horiz.png">		
+		<h1>Low Profile & Concealment Systems</h1>
+		<div class="menubar" id = "headerbar">	
+			<li><a href="/">Home</a></li>
+			<li><a href="about.php">About Us</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="form.php">Form</a></li>
 		</div>
-	
+	</div>
 </head>
 
 <body background="images/background.svg";>

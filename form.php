e3e6117c36494c44ec277d0afb5356c8ac072689
Mark Fontaine <Mark Fontaine>
2016-11-28 10:23:45 -0800

    adjust form h2

--- form.php
+++ form.php
@@ -3,7 +3,7 @@ $pageTitle = 'Order Form';
 include ('includes/header.php');
 ?>
 
-<h2> form</h2>
+<h2>Holster Custom Order Form</h2>
 <div class="form">
 	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdIbnwkgP4K6PthCa8BnS4SU4MkowfplSJLDTB4rPjvkqX9fg/viewform?embedded=true" 
 	width="760" height="1000" frameborder="0" marginheight="-50" marginwidth="0">Loading...</iframe>
